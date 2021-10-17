fdfb30dbfa6689fa2a6c3b2365ff3f7a840e5328
Michelle Brito <michele.brito17@gmail.com>
2021-10-17 14:37:50 -0300

    ADD a header screen

--- src/screens/CheckoutScreen.js
+++ src/screens/CheckoutScreen.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Text } from 'react-native-paper'
+import { StyleSheet } from 'react-native'
+import Header from '../components/Header'
+import BackButtonLight from '../components/BackButtonLight'
+import FormTitle from '../components/FormTitle'
+import Background from '../components/Background'
 
 export default function DetailsScreen({ route, navigation }) {
-  /* 2. Get the param */
   const { name, description, price, image } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  return (     
+    <Background>
+      <Header>
+        <BackButtonLight goBack={navigation.goBack} />
+      </Header> 
+        
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>      
+      <FormTitle> {JSON.stringify(name)}</FormTitle>
       <Text>name: {JSON.stringify(name)}</Text>
       <Text>description: {JSON.stringify(description)}</Text>
       <Text>price: {JSON.stringify(price)}</Text>
       <Text>image: {JSON.stringify(image)}</Text>
     </View>
+    </Background>
   );
 }
 
